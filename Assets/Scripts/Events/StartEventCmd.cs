8da60812212c534389b4eec743f04876011d2c9c
Michael Seltzer <mseltzer@alumni.upenn.edu>
2017-03-14 11:06:04 -0700

    Hooking events back into the game

---
+++ Assets/Scripts/Events/StartEventCmd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core;
+
+namespace DeWinter
+{
+	public class StartEventCommand : ICommand<string>
+	{
+		public void Execute(string eventName)
+		{
+/*
+			int randomRangeMax = 100;
+        //Intro Event
+        if (GameData.currentDay == 0)
+        {
+            screenFader.gameObject.SendMessage("CreateEventPopUp", "intro");
+        }
+        //All the Other Events
+        if (GameData.currentDay > 2 && (Random.Range(1, randomRangeMax + 1) < GameData.eventChance))
+        {
+            screenFader.gameObject.SendMessage("CreateEventPopUp", "night");
+        }
+*/
+		}
+	}
+}
