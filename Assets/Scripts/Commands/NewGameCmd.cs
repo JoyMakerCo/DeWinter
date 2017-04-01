8b334e1ff54162d647612268280a7cba8a579782
Michael Seltzer <mseltzer@alumni.upenn.edu>
2017-04-01 12:21:30 -0700

    Making initial parties

--- Assets/Scripts/Commands/NewGameCmd.cs
+++ Assets/Scripts/Commands/NewGameCmd.cs
@@ -9,6 +9,14 @@ namespace DeWinter
 		{
 			EventModel emod = DeWinterApp.GetModel<EventModel>();
 			emod.SelectedEvent = emod.eventInventories["intro"][0];
+
+			// Populate initial parties
+			CalendarModel cmod = DeWinterApp.GetModel<CalendarModel>();
+			cmod.Parties.Add(cmod.DaysFromNow(1), new Party(1));
+			cmod.Parties.Add(cmod.DaysFromNow(4), new Party(1));
+			if (new Random().Next(3) == 0)
+				cmod.Parties.Add(cmod.DaysFromNow(4), new Party(1));
+
 			DeWinterApp.SendMessage<string>(GameMessages.LOAD_SCENE, "Game_Estate");
 		}
 	}
