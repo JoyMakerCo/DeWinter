1c19fe90df82238ec4d18b8febd36e54f10df4a2
Michael Seltzer <mseltzer@alumni.upenn.edu>
2017-06-21 14:55:27 -0700

    Made the Dialog Button script easier to use

--- Assets/Scripts/UI/DialogButtonHandler.cs
+++ Assets/Scripts/UI/DialogButtonHandler.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DeWinter
 {
 	public class DialogButtonHandler : MonoBehaviour
 	{
-		public void OpenDialog (string DialogID)
+		public Dialog.DialogCanvasManager canvas;
+		public GameObject DialogPrefab;
+
+		protected Button _button;
+
+		void Start()
 		{
-			DeWinterApp.OpenDialog(DialogID);
+			_button = gameObject.GetComponent<Button>();
+			if (_button != null)
+				_button.onClick.AddListener(OnClick);
+		}
+
+		protected void OnClick()
+		{
+			canvas.Open(DialogPrefab);
 		}
 	}
 }
