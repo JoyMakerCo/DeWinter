8e74540c99ccb25c3aabb4b5cc9f7201d0e599f7
Michael Seltzer <mseltzer@alumni.upenn.edu>
2017-11-08 13:45:37 -0800

    Broke up guest/Host/Enemy names into first, last, and title

--- Assets/Scripts/Party/Commands/CreateEnemyCmd.cs
+++ Assets/Scripts/Party/Commands/CreateEnemyCmd.cs
@@ -18,30 +18,22 @@ namespace Ambition
 			EnemyInventory.enemyInventory.Add(e);
 
 			Random rnd = new Random();
-			string[] name;
 
 			e.Like = model.Interests[rnd.Next(model.Interests.Length)];
 
 			e.IsFemale = (rnd.Next(2) == 0);
 			if (e.IsFemale)
 			{
-				name = new string[]{
-					rndStr("female_title", rnd),
-					rndStr("female_name", rnd),
-					"de " + rndStr("last_name", rnd)
-				};
+				e.Title = rndStr("female_title", rnd);
+				e.FirstName = rndStr("female_name", rnd);
 			}
 			else
 			{
-				name = new string[]{
-					rndStr("male_title", rnd),
-					rndStr("male_name", rnd),
-					"de " + rndStr("last_name", rnd)
-				};
+				e.Title = rndStr("male_title", rnd);
+				e.FirstName = rndStr("male_name", rnd);
 			}
-			e.Name = string.Join(" ", name);
-			e.DisplayName = name[0] + " " + name[2];
 
+			e.LastName = rndStr("last_name", rnd);
 			e.imageInt = rnd.Next(e.IsFemale ? 4 : 5);
 			e.FlavorText = "This person is a great big jerk";
 		}
